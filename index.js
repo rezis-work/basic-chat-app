b14606b59d11d88de85a26e81a53e39053d315c9
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-05-17 15:36:20 +0400

    basic html express chat app

---
+++ index.js
@@ -0,0 +1,25 @@
+import http from "http";
+import express from "express";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("user connected successfully");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server is running on port http://localhost:5000");
+});
